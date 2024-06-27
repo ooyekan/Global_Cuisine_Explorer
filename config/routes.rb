9c13f29d3b512b3a5ca14706fc7fbbf0abdd3388
Oyebisi Oyekan <ooyekan@academic.rrc.ca>
2024-06-26 20:41:36 -0500

    Css styling added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
+  get 'about' => 'static_pages#about'
   get 'countries/index'
   get 'countries/show'
   resources :countries, only: [:index, :show] do
