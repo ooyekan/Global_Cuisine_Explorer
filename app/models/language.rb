b0f5b7925de85b4af9649c4b4060415b86210ee7
Oyebisi Oyekan <ooyekan@academic.rrc.ca>
2024-06-27 16:00:37 -0500

    All validations veified

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,4 +1,6 @@
 class Language < ApplicationRecord
+  validates :name, presence: true
+  validates :code, presence: true
   has_many :country_languages
   has_many :countries, through: :country_languages
 end
